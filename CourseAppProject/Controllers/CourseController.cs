da875a8941c78571ede998406e2b792ea5c58901
Melike Aslan <1melike2@gmail.com>
2022-06-10 02:58:28 +0300

    Add project files.

---
+++ CourseAppProject/Controllers/CourseController.cs
@@ -0,0 +1,43 @@
+﻿using CourseAppProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseAppProject.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            var course = new Course();
+            course.Name = "MVC Course";
+            course.Description = "güzel bir kurs";
+            course.IsPublished = true;
+
+            return View(course);
+        }
+
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Apply(string Name, string Email, string Phone, bool Confirm)
+        {
+            return View();
+        }
+    }
+}
---
+++ CourseAppProject/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseAppProject.Models
+{
+    public class Course
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsPublished { get; set; }
+    }
+}
